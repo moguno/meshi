4914275bd97048cb1b2f62636bba35716b065714
moguno <shopping@u.orz.ne.jp>
2014-07-08 23:17:24 +0900

    Update meshi.rb
    スペース入りのキーワードでもへっちゃらにした。

--- meshi.rb
+++ meshi.rb
@@ -7,12 +7,12 @@ Plugin.create(:meshi) do
   filter_gui_postbox_post do |gui_postbox|
     buf = Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer
     text = buf.text
-    if /<<([[:word:]]+)>>/ =~ text then
+    if /<<([^>]+)>>/ =~ text then
       uri = URI.escape("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+$1)
       response=open(uri)
       data = JSON.load(response.read)
       str = data["responseData"]["results"][0]["url"]
-      text[/<<([[:word:]]+)>>/,0] = str
+      text[/<<([^>]+)>>/,0] = str
     end
     buf.text = text
     [gui_postbox]
