3c75b4d9380891f8c0cedc2dc44bd4b93f627023
moguno <shopping@u.orz.ne.jp>
2014-07-09 00:59:08 +0900

    Update meshi.rb

--- meshi.rb
+++ meshi.rb
@@ -5,13 +5,16 @@ require 'json'
 
 Plugin.create(:meshi) do
   filter_gui_postbox_post do |gui_postbox|
+    puts "cccccc"
     buf = Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer
     text = buf.text
     if /<<([^>]+)>>/ =~ text then
       uri = URI.escape("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+$1)
       response=open(uri)
       data = JSON.load(response.read)
-      str = data["responseData"]["results"][0]["url"]
+      str = data["responseData"]["results"].find { |_| 
+      (_["unescapedUrl"] != nil) && _["unescapedUrl"] =~ /(jpg)$/ }["unescapedUrl"]
+
       text[/<<([^>]+)>>/,0] = str
     end
     buf.text = text
